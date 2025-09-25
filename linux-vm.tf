20bc3d81542d9f12f94f4be5db466817d4711f0f
ZeCyberMan2 <zecyberman@gmail.com>
2025-09-25 18:04:12 -0400

    Update linux-vm.tf

--- linux-vm.tf
+++ linux-vm.tf
@@ -29,30 +29,4 @@ resource "azurerm_network_interface" "networkinterface1" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "linux1" {
-  name                = "linux1-machine"
-  resource_group_name = azurerm_resource_group.linuxvm.name
-  location            = azurerm_resource_group.linuxvm.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.networkinterface1.id,
-  ]
 
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
